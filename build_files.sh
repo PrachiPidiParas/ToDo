c3fb07994154215279b1133546ab201ab3e35eac
PrachiPidiParas <141743737+PrachiPidiParas@users.noreply.github.com>
2024-08-03 19:32:25 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+# Ensure the correct version of Python is used
+export PATH="/vercel/path0/.python/bin:$PATH"
 python3.10  -m pip install -r requirements.txt
 python3.10 manage.py collectstatic --noinput --clear
 python manage.py migrate
