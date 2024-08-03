42d7b807d8927c2e081354b955ad6c4f596bbdb8
PrachiPidiParas <141743737+PrachiPidiParas@users.noreply.github.com>
2024-08-03 19:42:04 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -2,7 +2,7 @@
 
 # Ensure the correct version of Python is used
 export PATH="/vercel/path0/.python/bin:$PATH"
-python -m pip install -r requirements.txt
-python manage.py collectstatic --noinput --clear
-python manage.py migrate
+python-3.10.0 -m pip install -r requirements.txt
+python-3.10.0 manage.py collectstatic --noinput --clear
+python-3.10.0 manage.py migrate
 
